59cf88f4fde98cf797eb3d768165e155217bf1db
佐藤良昭 <yoshiprograming@gmail.com>
2020-07-12 23:51:36 +0900

    エラーの解決

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-    before_action :set_blog, only: [:show, :edit, :update]
+    before_action :set_task, only: [:show, :edit, :update]
     def index
       @tasks = Task.all
     end
